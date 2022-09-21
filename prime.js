42c6e379236ab680b938ff254c6f28336dd64b6f
bhargav <bhargavoutlook123@gmail.com>
2022-09-21 18:54:13 +0530

    code is correct

---
+++ prime.js
@@ -0,0 +1,21 @@
+function prime(num){
+
+var count = 0;
+
+    for(let i=0;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+
+    if(count>1){
+        console.log(num+"-"+"Prime")
+    }
+    else{
+        console.log("Not Prime");
+    }
+}
+
+let num = 5;
+
+prime(num);
